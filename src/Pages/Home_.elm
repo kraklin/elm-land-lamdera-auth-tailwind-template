310245b425d32e536bdf4b06f32c47221e9f2672
Elliot Fiske (L360) <efiske@life360.com>
2023-11-14 12:52:54 -0800

    Update button to Google brand guidelines

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -12,8 +12,6 @@ import Route exposing (Route)
 import Shared
 import Shared.Model
 import Shared.Msg
-import Svg
-import Svg.Attributes as SvgAttrs
 import View exposing (View)
 
 
@@ -113,28 +111,16 @@ view shared model =
                     ]
                     [ Html.div []
                         [ Html.div
-                            [ Attrs.class "mt-6 grid grid-cols-1 gap-4"
+                            [ Attrs.class "mt-6 flex justify-center"
                             ]
                             [ Html.button
                                 [ Attrs.href "#"
-                                , Attrs.class "flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+                                , Attrs.class "flex items-center justify-center gap-3 rounded-full px-3 py-1.5 border border-[#1F1F1F] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F] hover:bg-gray-100"
                                 , Events.onClick Login
                                 ]
-                                [ Svg.svg
-                                    [ SvgAttrs.class "h-5 w-5"
-                                    , SvgAttrs.fill "currentColor"
-                                    , SvgAttrs.viewBox "0 0 20 20"
-                                    , Attrs.attribute "aria-hidden" "true"
-                                    ]
-                                    [ Svg.path
-                                        [ SvgAttrs.fillRule "evenodd"
-                                        , SvgAttrs.d "M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                                        , SvgAttrs.clipRule "evenodd"
-                                        ]
-                                        []
-                                    ]
+                                [ Html.img [Attrs.src "google-logo.svg"] []
                                 , Html.span
-                                    [ Attrs.class "text-sm font-semibold leading-6"
+                                    [ Attrs.class "text-sm text-[#1F1F1F] font-semibold leading-6"
                                     ]
                                     [ Html.text "Sign in with Google" ]
                                 ]
