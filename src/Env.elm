a9615c37c6edd934f0632e7b66e502bd582d19cf
Tomáš  Látal <latal.tomas@gmail.com>
2023-11-15 10:39:05 +0100

    introduce Env.elm back

---
+++ src/Env.elm
@@ -0,0 +1,12 @@
+module Env exposing (..)
+
+-- The Env.elm file is for per-environment configuration.
+-- See https://dashboard.lamdera.app/docs/environment for more info.
+
+
+googleAppClientId =
+    ""
+
+
+googleAppClientSecret =
+    ""
