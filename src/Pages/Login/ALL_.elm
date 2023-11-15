88714771010605acba25c65c349297bba70dfe14
Tomáš  Látal <latal.tomas@gmail.com>
2023-11-15 09:56:05 +0100

    Add Login route so there is no Page not found fallback when getting back from Google Auth

---
+++ src/Pages/Login/ALL_.elm
@@ -0,0 +1,70 @@
+module Pages.Login.ALL_ exposing (Model, Msg, page)
+
+import Effect exposing (Effect)
+import Html
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
+import View exposing (View)
+
+
+page : Shared.Model -> Route { all_ : List String } -> Page Model Msg
+page shared route =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Logging you in"
+    , body = [ Html.text "Logging you in..." ]
+    }
